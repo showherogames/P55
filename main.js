a06cbe59f29ddf4b5aeb20e5b6f18367b9f387c4
SHOWHEROGAMES <idatenshow758@gmail.com>
2021-08-30 19:48:51 +0530

    deadstar sus

---
+++ main.js
@@ -0,0 +1,15 @@
+function function1() {
+    document.getElementById("audio1").play();
+}
+		function function2() {
+   
+}
+		function function3() {
+    
+}
+		function function4() {
+    
+}
+		function function5() {
+    
+}
