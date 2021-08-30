de47ba859a65cd4ffe89030b025debc1b549e241
SHOWHEROGAMES <idatenshow758@gmail.com>
2021-08-30 19:50:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ function function1() {
     document.getElementById("audio1").play();
 }
 		function function2() {
-   
+            document.getElementById("audio2").play();
 }
 		function function3() {
-    
+    document.getElementById("audio3").play();
 }
 		function function4() {
     
